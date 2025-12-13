a90e914e7db264421316a86e8adf710ba6a97863
alexandr1403 <zdravstvuyte123@gmail.com>
2025-12-13 11:15:18 +0300

    Добавил форму для добавления задач.

---
+++ issues/src/app/components/add-issue-component/add-issue.component.ts
@@ -0,0 +1,13 @@
+import { Component, Output, EventEmitter } from "@angular/core";
+
+@Component({
+    selector: 'app-add-form',
+    standalone: true,
+    imports: [],
+    templateUrl: 'add-issue.html',
+    styleUrls: ['add-issue.scss']
+})
+
+export class AddIssue {
+
+}
