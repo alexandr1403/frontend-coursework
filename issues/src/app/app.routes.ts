07630d6cab8be9526b782670f1be9487d8b6b7d6
alexandr1403 <zdravstvuyte123@gmail.com>
2025-12-13 09:54:49 +0300

    Добавлены пустые компоненты для работы с пользователем.

--- issues/src/app/app.routes.ts
+++ issues/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { IssueList } from './components/issue-list-component/issue-list.component';
 
 export const routes: Routes = [
-    {path: '', component: IssueList}
+    { path: '', component: IssueList }
 ];
