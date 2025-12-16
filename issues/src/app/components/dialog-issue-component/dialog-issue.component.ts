96b69debd099005b3c49ea46af0311a711618301
alexandr1403 <zdravstvuyte123@gmail.com>
2025-12-16 07:26:48 +0300

    Добавлен функционал в диалоговое окно. Обнаружены баги с регистрацией.

--- issues/src/app/components/dialog-issue-component/dialog-issue.component.ts
+++ issues/src/app/components/dialog-issue-component/dialog-issue.component.ts
@@ -1,23 +1,33 @@
 import { Component, Inject } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialogContent, MatDialogActions} from "@angular/material/dialog";
 import { IssueInterface } from "../../interfaces/issue.interface";
+import { FormsModule } from "@angular/forms";
+import { CommentService } from "../../services/comment.service";
 
 @Component ({
     selector: 'app-dialog',
     standalone: true,
-    imports: [MatDialogActions, MatDialogContent],
+    imports: [MatDialogActions, MatDialogContent, FormsModule],
     templateUrl: './dialog-issue.html',
     styleUrls: ['./dialog-issue.scss']
 })
 export class DialogComponent {
 
+    comment: string = '';
+
     constructor(public dialogRef: MatDialogRef<DialogComponent>, 
         @Inject(MAT_DIALOG_DATA) public data: { 
-            issue: IssueInterface  }) { } 
+            issue: IssueInterface  }, private service: CommentService) { } 
 
     closeDialog(): void {
         this.dialogRef.close('Диалог закрыт. ');
 
         console.log(this.data.issue.title);
+    }
+
+    commentIssue(): void {
+        const time = Date.now();
+        this.service.saveHistory(this.data.issue.id, time, this.data.issue.assigner.name, this.comment);
+        console.log("Комментарий оставлен: ", this.comment);
     }
 }
