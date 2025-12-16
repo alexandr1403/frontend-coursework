252c7543858d7113cdcc37b40d8f35d17f6bd85c
alexandr1403 <zdravstvuyte123@gmail.com>
2025-12-16 09:53:48 +0300

    Добавил сервис задач.

--- issues/src/app/services/issue.service.ts
+++ issues/src/app/services/issue.service.ts
@@ -1,1 +1,22 @@
-// здесь предполагается сохранение списка задач в localStorage 
+import { Injectable } from "@angular/core";
+import { IssueInterface } from "../interfaces/issue.interface";
+
+@Injectable({
+    providedIn: 'root',
+})
+
+export class IssueService {
+
+    private readonly KEY = 'issue-list';
+
+    getIssues(): IssueInterface[] {
+        const issues = localStorage.getItem(this.KEY);
+        return (issues)? JSON.parse(issues) : [];
+    }
+
+    saveIssues(issue: IssueInterface): void {
+        const issues = this.getIssues();
+        issues.push(issue);
+        localStorage.setItem(this.KEY, JSON.stringify(issues));
+    }
+}
