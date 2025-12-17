001248f194a6d0b7701bb4123feef8cb8b469d3e
alexandr1403 <zdravstvuyte123@gmail.com>
2025-12-17 21:31:15 +0300

    Исправил ошибку в тесте сервиса задач, добавил туда пару тестов.

--- issues/src/app/services/issue-service/issue-service.spec.ts
+++ issues/src/app/services/issue-service/issue-service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from "@angular/core/testing";
 import { IssueService } from "./issue.service";
-import { Inject } from "@angular/core";
+import { UserInterface } from "../../interfaces/user.interface";
 
 describe('IssueService', () => {
     let service: IssueService;
@@ -9,10 +9,36 @@ describe('IssueService', () => {
         TestBed.configureTestingModule({
             providers: [IssueService],
         });
-        service = Inject(IssueService);
+        service = TestBed.inject(IssueService);
     })
 
     it('should be created', () => {
         expect(service).toBeTruthy();
-    })   
+    })
+
+    it('should be closed', () => {
+        const gettingShpion = spyOn(service, 'getClosed');
+        service.closeIssue(0);
+
+        expect(gettingShpion).toHaveBeenCalled();
+    })
+
+    it('should be saved', () => {
+        const gettingShpion = spyOn(service, 'saveIssues');
+        service.closeIssue(0);
+
+        expect(gettingShpion).toHaveBeenCalled();
+    })
+
+    const mockUser: UserInterface = {
+        id: 0,
+        name: "alex",
+        password: "passw",
+    };
+
+    it('should be false assigning', () => {
+        const realData = service.assign(0, mockUser);
+
+        expect(realData).toEqual(false);
+    })
 })
